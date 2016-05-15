21f93af10e0e89a3db869bc26ede231059c72e4c
eugene golovanov <eugene.golovanov@hart.com>
2016-05-15 02:11:44 -0700

    hart refreshing started

---
+++ PullToRefresh/HartRefreshView.swift
@@ -0,0 +1,163 @@
+//
+//  HartRefreshView.swift
+//  PullToRefresh
+//
+//  Created by eugene golovanov on 5/15/16.
+//  Copyright © 2016 eugene golovanov. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import Foundation
+
+
+//-------------------------------------------------------------------------------------------------------------
+//MARK: protocol
+
+protocol HartRefreshViewDelegate {
+    func refreshViewDidRefresh(refreshView: HartRefreshView)
+}
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//MARK: class
+
+class HartRefreshView: UIView, UIScrollViewDelegate {
+    
+    //-------------------------------------------------------------------------------------------------------------
+    //MARK: properties
+    
+    var delegate: HartRefreshViewDelegate?
+    var scrollView: UIScrollView?
+    var refreshing: Bool = false
+    var progress: CGFloat = 0.0
+    
+    var isRefreshing = false
+    
+    let ovalShapeLayer: CAShapeLayer = CAShapeLayer()
+    let airplaneLayer: CALayer = CALayer()
+    let textLayer = CATextLayer()
+    
+    
+    
+    //-------------------------------------------------------------------------------------------------------------
+    //MARK: init
+    init(frame: CGRect, scrollView: UIScrollView) {
+        super.init(frame: frame)
+        
+        self.scrollView = scrollView
+        
+        //add the background
+        
+        self.backgroundColor = UIColor.darkGrayColor()
+//        addSubview(UIImageView(image: UIImage(named: "refresh-view-bg")))
+        
+        
+        //OVAL REFRESH
+        ovalShapeLayer.strokeColor = UIColor.whiteColor().CGColor
+        ovalShapeLayer.fillColor = UIColor.clearColor().CGColor
+        ovalShapeLayer.lineWidth = 4.0
+        ovalShapeLayer.lineDashPattern = [2, 3]
+        let refreshRadius = frame.size.height/2 * 0.8
+        ovalShapeLayer.path = UIBezierPath(ovalInRect: CGRect(
+            x: frame.size.width/2 - refreshRadius,
+            y: frame.size.height/2 - refreshRadius,
+            width: 2 * refreshRadius,
+            height: 2 * refreshRadius)
+            ).CGPath
+        
+        layer.addSublayer(ovalShapeLayer)
+
+        
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    //-------------------------------------------------------------------------------------------------------------
+    //MARK: - Stroke
+    
+    func redrawFromProgress() {
+        ovalShapeLayer.strokeEnd = self.progress
+        airplaneLayer.opacity = Float(self.progress)
+    }
+
+
+    
+    
+    //-------------------------------------------------------------------------------------------------------------
+    //MARK: Scroll View Delegate methods
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+
+        let refreshViewVisibleHeight = max(0, -(scrollView.contentOffset.y + scrollView.contentInset.top))
+        self.progress = min(max(refreshViewVisibleHeight/frame.size.height, 0.0), 1.0)
+        print("progressPercentage = \(self.progress)")
+        
+        if !isRefreshing {
+            redrawFromProgress()
+        }
+
+
+    }
+    
+    func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        if !isRefreshing && self.progress >= 1.0 {
+            delegate?.refreshViewDidRefresh(self)
+            beginRefreshing()
+        }
+        
+    }
+    
+    //-------------------------------------------------------------------------------------------------------------
+    //MARK: animate the Refresh View
+    
+    func beginRefreshing() {
+        isRefreshing = true
+        
+        UIView.animateWithDuration(0.3, animations: {
+            var newInsets = self.scrollView!.contentInset
+            newInsets.top += self.frame.size.height
+            self.scrollView!.contentInset = newInsets
+        })
+        
+        
+        //Refreshing Animation
+        let strokeStartAnimation = CABasicAnimation(keyPath: "strokeStart")
+        strokeStartAnimation.fromValue = -0.5
+        strokeStartAnimation.toValue   = 1.0
+        
+        let strokeEndAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        strokeEndAnimation.fromValue = 0.0
+        strokeEndAnimation.toValue = 1.0
+        
+        let strokeAnimationGroup = CAAnimationGroup()
+        strokeAnimationGroup.duration = 1.5
+        strokeAnimationGroup.repeatDuration = 5.0
+        strokeAnimationGroup.animations = [strokeStartAnimation, strokeEndAnimation]
+        
+        ovalShapeLayer.addAnimation(strokeAnimationGroup, forKey: nil)
+
+        
+    }
+    
+    func endRefreshing() {
+        
+        isRefreshing = false
+        
+        UIView.animateWithDuration(0.3, delay:0.0, options: .CurveEaseOut ,animations: {
+            var newInsets = self.scrollView!.contentInset
+            newInsets.top -= self.frame.size.height
+            self.scrollView!.contentInset = newInsets
+            }, completion: {_ in
+                //finished
+        })
+    }
+
+
+}
--- PullToRefresh/HartTVC.swift
+++ PullToRefresh/HartTVC.swift
@@ -1,65 +1,60 @@
 //
-//  RefreshTVC.swift
+//  HartTVC.swift
 //  PullToRefresh
 //
-//  Created by eugene golovanov on 5/14/16.
+//  Created by eugene golovanov on 5/15/16.
 //  Copyright © 2016 eugene golovanov. All rights reserved.
 //
 
 import UIKit
 
-
-private let refreshViewHeight:CGFloat = 200
-
-func delayBySeconds(seconds: Double, delayedCode: ()->()) {
-    let targetTime = dispatch_time(DISPATCH_TIME_NOW, Int64(Double(NSEC_PER_SEC) * seconds))
-    dispatch_after(targetTime, dispatch_get_main_queue()) {
-        delayedCode()
-    }
-}
+let kRefreshViewHeight:CGFloat = 110.0
 
 
-class RefreshTVC: UITableViewController, RefreshViewDelegate {
-
-    var refreshView:RefreshView!
+class HartTVC: UITableViewController, HartRefreshViewDelegate {
+    
+    var hartRefreshView:HartRefreshView!
     
     
     
     //-------------------------------------------------------------------------------------------------------------
     // MARK: - View Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //creating refreshView
-        refreshView = RefreshView(frame: CGRect(x: 0, y: -refreshViewHeight, width: CGRectGetWidth(self.view.bounds), height: refreshViewHeight), scrollView: tableView)
-        refreshView.translatesAutoresizingMaskIntoConstraints = false
-        refreshView.delegate = self
-        view.insertSubview(refreshView, atIndex: 0)
+        let refreshRect = CGRect(x: 0.0, y: -kRefreshViewHeight, width: CGRectGetWidth(self.view.bounds), height: kRefreshViewHeight)
+        hartRefreshView = HartRefreshView(frame: refreshRect, scrollView: self.tableView)
+        hartRefreshView.delegate = self
+        view.addSubview(hartRefreshView)
     }
-
+    
+    
+    
     //-------------------------------------------------------------------------------------------------------------
     // MARK: - -Scroll View delegate methods-
-
+    
     
     
     //This Function forwarding scrolling of tableView into refreshView
     override func scrollViewDidScroll(scrollView: UIScrollView) {
-        refreshView.scrollViewDidScroll(scrollView)
+        hartRefreshView.scrollViewDidScroll(scrollView)
     }
     
     //This Function forwarding end of scrolling of tableView into refreshView
     override func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        refreshView.scrollViewWillEndDragging(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
+        hartRefreshView.scrollViewWillEndDragging(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
     }
+    
+    
     
     
     //-------------------------------------------------------------------------------------------------------------
     // MARK: - -RefreshView delegate method-
     
-    func refreshViewDidRefresh(refreshView: RefreshView) {
-        delayBySeconds(3) { 
-            self.refreshView.endRefresh()
+    func refreshViewDidRefresh(refreshView: HartRefreshView) {
+        delayBySeconds(3) {
+            self.hartRefreshView.endRefreshing()
         }
     }
     
@@ -67,25 +62,25 @@ class RefreshTVC: UITableViewController, RefreshViewDelegate {
     
     //-------------------------------------------------------------------------------------------------------------
     // MARK: - -Table view data source-
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 50
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseHartIdentifier", forIndexPath: indexPath)
+        
         // Configure the cell...
-        cell.textLabel!.text = "cell \(indexPath.row)"
+        cell.textLabel!.text = "hart cell \(indexPath.row)"
         return cell
     }
- 
-
-
+    
+    
+    
 }
